c3f6aa7f99b49651e5f39716f745fe6bdaf90f5a
therealsharath <palathingal@gatech.edu>
2020-09-15 19:19:13 -0400

    Initial commit

---
+++ mapscout-homepage/src/routes/index.svelte
@@ -0,0 +1,17 @@
+<style>
+</style>
+
+<script>
+	let count = 0
+	function handleClick(param) {
+		
+	}
+</script>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<button>+</button>
+<button>-</button>
+{count}
