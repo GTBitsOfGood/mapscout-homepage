25064ada4070e71b6aeb0e58ef6a7625ced93c53
therealsharath <palathingal@gatech.edu>
2020-09-18 03:59:25 -0400

    Bare bone template for about page

--- mapscout-homepage/src/routes/index.svelte
+++ mapscout-homepage/src/routes/index.svelte
@@ -2,16 +2,8 @@
 </style>
 
 <script>
-	let count = 0
-	function handleClick(param) {
-		
-	}
 </script>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	
 </svelte:head>
-
-<button>+</button>
-<button>-</button>
-{count}
