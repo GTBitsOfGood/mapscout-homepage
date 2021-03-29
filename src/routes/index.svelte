65f5307748ff706554134d9475cd8c85cfea5f12
Sjoyner8 <45107110+savannahjoyner@users.noreply.github.com>
2021-03-28 23:39:12 -0400

    prismic styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,16 +23,46 @@
 </script>
 
 <style>
-	.padding {
-		height: 16px;
-	}
 
-	section {
+ section {
 		min-height: 100vh;
 		width: 100%;
 		display: flex;
 		align-items: center;
-		justify-content: center;
+		justify-content: space-between;
+    padding: 0% 10%;
+	}
+
+  .btn-primary{
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    padding: 8px 12px;
+    left: 15.78%;
+    right: 73.31%;
+    top: 9.91%;
+    bottom: 89.28%;
+    background: #0A1D7C;
+    border-radius: 11.2203px;
+    border-color: none;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 24px;
+    text-align: center;
+  }
+  .image {
+
+    /* height: 100%; */
+  }
+  .text-wrapper{
+    display: inline-block; 
+    white-space:pre-wrap;
+    /* word-wrap: normal; */
+  }
+
+	.padding {
+		height: 16px;
 	}
 
 	.bg-image {
@@ -41,19 +71,25 @@
 		background-position: right;
 	}
 
-	.wrapper {
-		width: 65%;
+	/* .wrapper {
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
-	}
+	} */
 
 	div {
 		width: 30vw;
 	}
 
 	h1 {
-		font-weight: 700;
+    left: 15.69%;
+    right: 48.12%;
+    top: 5.7%;
+    bottom: 92.47%;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 44.3404px;
+    line-height: 53px;
 	}
 
 	.alternate-text {
@@ -70,12 +106,19 @@
 	}
 
 	p {
-		font-size: 1.3em;
+    left: 15.69%;
+    right: 55.28%;
+    top: 7.98%;
+    bottom: 90.39%;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 22.1702px;
+    line-height: 27px;
 	}
 
 	.placeholder {
-		height: 45vh;
-		width: 30vw;
+    width: 100%;
+    object-fit: contain;
 		background: #C4C4C4;
 		filter: drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.15));
 	}
@@ -103,17 +146,33 @@
 
 	@media only screen and (max-width: 768px) {
 		h1 {
-			font-size: 10vw;
+      left: 10.13%;
+      right: -15.03%;
+      top: 54.12%;
+      bottom: 34.71%;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 33.4792px;
+      line-height: 40px;
 		}
 
 		p {
-			font-size: 1.1em;
+      left: 10.13%;
+      right: 5.7%;
+      top: 68.05%;
+      bottom: 21.99%;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 16.7396px;
+      line-height: 20px;
+
 		}
 
-		.wrapper {
-			width: 90%;
+		section {
+      justify-content: center;
 			flex-direction: column-reverse;
 			align-items: space-between;
+      
 		}
 
 		div {
@@ -121,8 +180,8 @@
 		}
 
 		.placeholder {
-			height: 30vh;
-			width: 70vw;
+		  width: 100%;
+      margin-bottom: 20px;
 			background: #C4C4C4;
 		}
 
@@ -161,23 +220,53 @@
 
 <div class="padding">&nbsp</div>
 {#if data}
-	{#each data as item}
-		<section class={alternate}>
-			<div class="wrapper">
-				<div>
+	{#each data as item, i}
+    {#if i<=3}
+		<section class= "{i%2 === 1 ? 'alternate' : ''}" >
+				<div class="text-wrapper">
 					<h1>{item.primary.title[0].text}</h1>
 					<p>{item.primary.description[0].text}</p>
+          {#if i == 0}
 					<button class="btn btn-primary">Get Started</button>
+          {/if}
 				</div>
-				<div class="buffer">&nbsp</div>
+        <div class="image">
 				<img class="placeholder" src="/favicon.png" alt="place_holder"/>
-			</div>
+        </div>
 		</section>
-		{#if alternate == "alternate"} {alternate = ""} 
-		{:else} {alternate = ""} {/if}
+    {/if}
+    {#if i>3}
+    <section>
+      <div class="main-disc">
+        <div class="normal">
+          <h1>{item.primary.title[0].text}</h1>
+          <p> {item.primary.description[0].text}</p>
+        </div>
+        <div class="card-container">
+          <Card 
+				image="/favicon.png"
+				title="The Philadelphia Alliance for Child Trauma Services (PACTS)" 
+				discription="PACTS is a network of child serving systems and organizations that provides evidence-based practices for traumatized youth and their families in the greater Philadelphia area."
+			/>
+			<Card 
+				image="/favicon.png"
+				title="ClinWiki" 
+				discription="A 501(c)(3) focused on making clinical trials more transparent and approachable in order to drive participation and faster progress against serious diseases."
+			/>
+			<Card 
+				image="/favicon.png"
+				title="Georgia DBHDD" 
+				discription="Organization description Organization
+				description Organization description Organization description Organization description Organization description"
+			/>
+        </div>
+        <div class="buffer">&nbsp</div>
+      </div>		
+    </section>
+    {/if}
 	{/each}
 {/if}
-<section class="alternate">
+<!-- <section class="alternate">
 	<div class="wrapper">
 		<div>
 			<h1>Easily add, edit, and delete resources</h1>
@@ -214,7 +303,7 @@
 		<div class="buffer">&nbsp</div>
 		<img class="placeholder" src="/favicon.png" alt="place_holder"/>
 	</div>
-</section>
+</section> -->
 <section>
 	<div class="main-disc">
 		<div class="normal">
